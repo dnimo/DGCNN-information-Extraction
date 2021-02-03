ed8a9670de5600cef891d37a60c1af70f58c1a68
yumasano <masanozgq@gmail.com>
2021-02-03 22:06:43 +0800

    进一步解析了python读取json文件的方法，但是用到了多个for循环嵌套，待优化，一种方案是将文件中的信息全部读取出来之后组成存的文件，方案待进一步跟进解决！

--- load_data.py
+++ load_data.py
@@ -4,37 +4,42 @@ import json
 from tqdm import tqdm
 import codecs
 
-all_50_schemas = []
+all_50_schemas = {}
 
-with open('data/example_50.json','r',encoding='utf-8') as f:
+with open('data/example_50.json', 'r', encoding='utf-8') as f:
     for l in tqdm(f):
         a = json.loads(l)
-        all_50_schemas.extend(a['qas'])
+        for i in a['qas']:
+            for j in i:
+                if j['question'] == '结果中的核心名词':
+                    for k in j['answers']:
+                        print(k['text'])
+
 
 # 整理之后的json文档
-with codecs.open('data/test.json','w',encoding='utf-8') as f:
-    json.dump(all_50_schemas, f, indent=4, ensure_ascii=False)
+# with codecs.open('data/test.json', 'w', encoding='utf-8') as f:
+#     json.dump(all_50_schemas, f, indent=4, ensure_ascii=False)
 
-chars = {}
-min_count = 2
+# chars = {}
+# min_count = 2
 
 # 创建训练数据集
-train_data = []
-with open('data/sentence_800') as f:
-    for l in tqdm(f):
-        a = json.loads(l)
-        if not a['spo_list']:
-            continue
-        train_data.append(
-            {
-                'text':a['text'],
-                'spo_list' : [(i['subject'], i['predicate'], i['object']) for i in a['spo_list']]
-            }
-        )
-        for c in a['text']:
-            chars[c] = chars.get(c, 0) + 1
-
-with codecs.open('data/train_data_me.json', 'w', encoding='utf-8') as f:
-    json.dump(train_data, f, indent=4, ensure_ascii=False)
+# train_data = []
+# with open('data/sentence_800') as f:
+#     for l in tqdm(f):
+#         a = json.loads(l)
+#         if not a['spo_list']:
+#             continue
+#         train_data.append(
+#             {
+#                 'text': a['text'],
+#                 'spo_list': [(i['subject'], i['predicate'], i['object']) for i in a['spo_list']]
+#             }
+#         )
+#         for c in a['text']:
+#             chars[c] = chars.get(c, 0) + 1
+#
+# with codecs.open('data/train_data_me.json', 'w', encoding='utf-8') as f:
+#     json.dump(train_data, f, indent=4, ensure_ascii=False)
 
 # 创建自己的测试数据集 dev_data.json
