bf2cc4348340c683e4eef172cedbce8e69e9e0f9
yumasano <masanozgq@gmail.com>
2021-02-17 21:18:37 +0800

    完成了位置特征向量的构建

--- trainsfor.py
+++ trainsfor.py
@@ -3,8 +3,9 @@ from tqdm import tqdm
 import codecs
 
 train = []
+chars = {}
 
-with open('data/example_50.json', 'r', encoding='utf-8') as f:
+with open('data/train_data.json', 'r', encoding='utf-8') as f:
     for l in tqdm(f):
         a = json.loads(l)
         for doc in a['document']:
@@ -19,9 +20,16 @@ with open('data/example_50.json', 'r', encoding='utf-8') as f:
                     'spo_list': tmp
                 }
             )
-
-print(train)
+        for c in doc['text']:
+            chars[c] = chars.get(c, 0) + 1
 
 # 整理之后的json文档
-with codecs.open('data/test00.json', 'w', encoding='utf-8') as f:
+with codecs.open('data/train_data_me.json', 'w', encoding='utf-8') as f:
     json.dump(train, f, indent=4, ensure_ascii=False)
+
+# 位置特征
+with codecs.open('data/all_chars_me.json', 'w', encoding='utf-8') as f:
+    chars = {i:j for i,j in chars.items() if j >= 2}
+    id2char = {i+2:j for i,j in enumerate(chars)}
+    char2id = {j:i for i,j in id2char.items()}
+    json.dump([id2char, char2id], f, indent=4, ensure_ascii=False)
